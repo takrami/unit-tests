c022883769a921604339e4a5100bd3813a9d9d5f
Atefeh H.Takrami <ati.takrami@gmail.com>
2021-03-01 19:27:46 +0100

    fix filterNumbers function

--- src/6-filter-numbers.js
+++ src/6-filter-numbers.js
@@ -1,3 +1,10 @@
-export const filterNumbers = (array, largerThan) => {
-  return array
-}
+export const filterNumbers = (array = [1, 20, 25, 34, 99], largerThan = 25) => {
+  const filteredArray = [];
+  array.map((number) => {
+    if (number <= largerThan) {
+      filteredArray.push(number);
+    }
+  });
+  return filteredArray;
+};
+filterNumbers();
