aa93891640fc50012769df3172be458853a29407
Atefeh H.Takrami <ati.takrami@gmail.com>
2021-03-02 11:32:14 +0100

    fix hashtags function

--- src/9-hashtags.js
+++ src/9-hashtags.js
@@ -1,3 +1,12 @@
-export const hashtags = (text) => {
-  return text
-}
+export const hashtags = (
+  text = "Hello #world this is a #string with #hashtags"
+) => {
+  const res = text.split(" ");
+  const hashtags = [];
+  res.map((item) => {
+    if (item[0] === "#") {
+      hashtags.push(item);
+    }
+  });
+  return hashtags;
+};
